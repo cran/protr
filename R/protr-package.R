112860ec6b6fa19a2e6ebeb00121a4cfd7e7e571
Nan Xiao <road2stat@gmail.com>
2013-12-10 00:00:00 +0000

    version 0.2-0

--- R/protr-package.R
+++ R/protr-package.R
@@ -1,78 +1,60 @@
-#' Protein Sequence Feature Extraction with R
+#' Protein Sequence Descriptor Calculation and Similarity Computation with R
 #'
-#' The protr package aims for protein sequence feature extraction, 
-#' which could be easily applied in Chemoinformatics, Bioinforamtics 
-#' and Chemogenomics research. The features listed in this package 
-#' include Amino Acid Composition (Amino Acid Composition/Dipeptide 
-#' Composition/Tripeptide Composition), Autocorrelation (Normalized 
-#' Moreau-Broto Autocorrelation/Moran Autocorrelation/Geary 
-#' Autocorrelation), CTD (Composition/Transition/Distribution), 
-#' Conjoint Traid, Quasi-sequence Order (Sequence Order Coupling 
-#' Number/Quasi-sequence Order Descriptors), and Pseudo Amino Acid 
-#' Composition (Pseudo Amino Acid Composition/Amphiphilic Pseudo 
-#' Amino Acid Composition). The package is developed by Computational 
-#' Biology and Drug Design Group, Central South University.
-#'
+#' The protr package focus on offering a unique and comprehensive toolkit 
+#' for protein sequence descriptor calculation and similarity computation. 
+#' The descriptors included in the protr package are extensively utilized 
+#' in Bioinformatics and Chemogenomics research. The qualitative descriptors 
+#' listed in protr include Amino Acid Composition (Amino Acid 
+#' Composition/Dipeptide Composition/Tripeptide Composition) descriptor, 
+#' Autocorrelation (Normalized Moreau-Broto Autocorrelation/Moran 
+#' Autocorrelation/Geary Autocorrelation) descriptor, CTD 
+#' (Composition/Transition/Distribution) descriptor, Conjoint Traid descriptor, 
+#' Quasi-sequence Order (Sequence Order Coupling Number/Quasi-sequence Order 
+#' Descriptors) descriptor and Pseudo Amino Acid Composition (Pseudo Amino 
+#' Acid Composition/Amphiphilic Pseudo Amino Acid Composition) descriptor. 
+#' The quantitative descriptors, for Proteochemometric (PCM) Modeling, 
+#' includes the Generalized Scales-Based Descriptors derived by Principal 
+#' Components Analysis, Generalized Scales-Based Descriptors derived by 
+#' AA-Properties (AAindex), Generalized Scales-Based Descriptors derived 
+#' by 20+ classes of 2D and 3D Molecular Descriptors (Topological, 
+#' WHIM, VHSE, etc.), Generalized Scales-Based Descriptors derived by 
+#' Factor Analysis, Generalized Scales-Based Descriptors derived by 
+#' Multidimensional Scaling, and Generalized BLOSUM/PAM Matrix-Derived 
+#' Descriptors. The protr package also integrates the functionality of 
+#' parallellized similarity computation derived by protein sequence alignment 
+#' and Gene Ontology (GO) semantic similarity measures between a list of 
+#' protein sequences / GO terms / Entrez Gene IDs. ProtrWeb, the web service 
+#' built on protr, is located at: http://cbdd.csu.edu.cn:8080/protrweb/ . 
+#' The protr package is developed by Computational Biology and Drug Design 
+#' (CBDD) Group, Central South University.
+#' 
 #' \tabular{ll}{ Package: \tab protr\cr Type: \tab Package\cr
-#' Version: \tab 0.1-0\cr License: \tab BSD\cr }
-#'
+#' Version: \tab 0.2-0\cr License: \tab BSD 3-clause License\cr }
+#' 
 #' @name protr-package
 #' @aliases protr
 #' @docType package
 #' @exportPattern "^[^\\.]"
-#' @author Xiao Nan <\email{road2stat@@gmail.com}>
-#'         Dongsheng Cao \email{oriental-cds@@163.com}
-#'         Qingsong Xu \email{dasongxu@@gmail.com}
-#'         Yizeng Liang \email{yizeng_liang@@263.net}
+#' @author Nan Xiao <\email{road2stat@@gmail.com}>
+#'         Qingsong Xu <\email{dasongxu@@gmail.com}>
+#'         Dongsheng Cao <\email{oriental-cds@@163.com}>
 #' 
 #' @note
 #' The comprehensive user's guide could be opened with \code{vignette('protr')},
 #' which explains each descriptor included in this package and corresponding
 #' usage.
 #' 
+#' The web interface for this package, \code{ProtrWeb} is located at:
+#' \url{http://cbdd.csu.edu.cn:8080/protrweb/}.
+#' 
 #' Bug reports and feature requests should be sent to
 #' \url{https://github.com/road2stat/protr/issues}.
 #' 
 #' @references
-#' The associated website for this package:
-#' \url{http://cbdd.csu.edu.cn/protr/}
-#'
-#' @seealso See \code{\link{extractAAC}}, 
-#'              \code{\link{extractDC}}, \code{\link{extractTC}},
-#'              \code{\link{extractMoreauBroto}}, \code{\link{extractMoran}},
-#'              \code{\link{extractGeary}}, \code{\link{extractCTDC}},
-#'              \code{\link{extractCTDT}}, \code{\link{extractCTDD}},
-#'              \code{\link{extractCTriad}}, \code{\link{extractSOCN}},
-#'              \code{\link{extractQSO}}, \code{\link{extractPAAC}},
-#'              \code{\link{extractAPAAC}} for details.
+#' (to appear)
 #' 
 #' @keywords protr package protein sequence amino acid feature extraction
 #'           descriptors chemoinformatics bioinforamtics chemogenomics
 #' 
 #' @example inst/examples/protr-package-ex.R
 NULL
-
-#' AAindex Data of 544 Physicochemical and Biological Properties
-#' for 20 Amino Acids
-#' 
-#' AAindex Data of 544 Physicochemical and Biological Properties
-#' for 20 Amino Acids
-#' 
-#' The data was extracted from the AAindex1 database ver 9.1 
-#' (\url{ftp://ftp.genome.jp/pub/db/community/aaindex/aaindex1}) as of Nov. 2012
-#' (Data Last Modified 2006-08-14).
-#' 
-#' With this data, users could investigate each property's accession number 
-#' and other details. Visit \url{http://www.genome.jp/dbget/aaindex.html} 
-#' for more information.
-#' 
-#' @docType data
-#' @name AAindex
-#' @aliases AAindex
-#' @usage data(AAindex)
-#' @keywords AAindex aaindex datasets
-#' @examples
-#' data(AAindex)
-#' 
-NULL
-
