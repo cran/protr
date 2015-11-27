fd2326a24f16197be5b1ccc12a112903425ae5e9
Nan Xiao <road2stat@gmail.com>
2015-11-27 11:41:14 +0000

    version 1.0-1

--- inst/doc/protr.R
+++ inst/doc/protr.R
@@ -3,7 +3,7 @@
 ###################################################
 ### code chunk number 1: prelim
 ###################################################
-protr.version = '0.5-1'
+protr.version = '1.0-1'
 now.date = strftime(Sys.Date(), "%Y-%m-%d")
 
 
@@ -40,16 +40,16 @@ head(moreau, n = 36L)
 ### code chunk number 6: extractMoreau2
 ###################################################
 # Define 3 custom properties
-myprops = data.frame(AccNo = c("MyProp1", "MyProp2", "MyProp3"), 
-                     A = c(0.62,  -0.5, 15),  R = c(-2.53,   3, 101), 
-                     N = c(-0.78,  0.2, 58),  D = c(-0.9,    3, 59), 
-                     C = c(0.29,    -1, 47),  E = c(-0.74,   3, 73), 
-                     Q = c(-0.85,  0.2, 72),  G = c(0.48,    0, 1), 
-                     H = c(-0.4,  -0.5, 82),  I = c(1.38, -1.8, 57), 
-                     L = c(1.06,  -1.8, 57),  K = c(-1.5,    3, 73), 
-                     M = c(0.64,  -1.3, 75),  F = c(1.19, -2.5, 91), 
-                     P = c(0.12,     0, 42),  S = c(-0.18, 0.3, 31), 
-                     T = c(-0.05, -0.4, 45),  W = c(0.81, -3.4, 130), 
+myprops = data.frame(AccNo = c("MyProp1", "MyProp2", "MyProp3"),
+                     A = c(0.62,  -0.5, 15),  R = c(-2.53,   3, 101),
+                     N = c(-0.78,  0.2, 58),  D = c(-0.9,    3, 59),
+                     C = c(0.29,    -1, 47),  E = c(-0.74,   3, 73),
+                     Q = c(-0.85,  0.2, 72),  G = c(0.48,    0, 1),
+                     H = c(-0.4,  -0.5, 82),  I = c(1.38, -1.8, 57),
+                     L = c(1.06,  -1.8, 57),  K = c(-1.5,    3, 73),
+                     M = c(0.64,  -1.3, 75),  F = c(1.19, -2.5, 91),
+                     P = c(0.12,     0, 42),  S = c(-0.18, 0.3, 31),
+                     T = c(-0.05, -0.4, 45),  W = c(0.81, -3.4, 130),
                      Y = c(0.26,  -2.3, 107), V = c(1.08, -1.5, 43))
 
 # Use 4 properties in the AAindex database, and 3 cutomized properties
@@ -137,8 +137,8 @@ extractAPAAC(x)
 ### code chunk number 17: extractDescScales
 ###################################################
 x = readFASTA(system.file('protseq/P00750.fasta', package = 'protr'))[[1]]
-descscales = extractDescScales(x, propmat = 'AATopo', 
-                               index = c(37:41, 43:47), 
+descscales = extractDescScales(x, propmat = 'AATopo',
+                               index = c(37:41, 43:47),
                                pc = 5, lag = 7, silent = FALSE)
 
 
@@ -153,7 +153,7 @@ head(descscales, 15)
 ### code chunk number 19: extractBLOSUM
 ###################################################
 x = readFASTA(system.file('protseq/P00750.fasta', package = 'protr'))[[1]]
-blosum = extractBLOSUM(x, submat = 'AABLOSUM62', 
+blosum = extractBLOSUM(x, submat = 'AABLOSUM62',
                        k = 5, lag = 7, scale = TRUE, silent = FALSE)
 
 
